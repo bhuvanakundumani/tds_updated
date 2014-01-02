4d967701a9fef091714e11625f4f3631f28da767
bhuvanakundumani <bhuvana.kundumani@gmail.com>
2014-01-02 22:14:25 +0530

    Initialize repository

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def programs
+  end
+
+  def aboutus
+  end
+
+  def gallery
+  end
+
+  def contactus
+  end
+    
+ 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tds::Application.config.secret_key_base = 'dadca3c1b94295c934d6a6d9d460e3a946b0f8c6ee3956db0b0183cacdf2be3652b23b0109f8fc819bddbf0301a7a4846e97a46b01bf65422d3c22aa982b0ea8'
